899423ef65c049752ccbab9382dda91df5438642
Partab Raj <64382440+smmartprogrammer@users.noreply.github.com>
2020-08-02 05:09:40 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,38 +1,46 @@
 var list = document.getElementById("list");
 
 
-function addTodo(){
+function addTodo() {
     var todo_item = document.getElementById("todo-item");
 
     // create li tag with text node
     var li = document.createElement('li')
     var liText = document.createTextNode(todo_item.value)
+    li.classList.add("fa", "fa-check-square-o", "fa-6");
     li.appendChild(liText)
-    
+
+
+
 
     // create delete button
     var delBtn = document.createElement("button")
-    var delText = document.createTextNode("Delete")
+    delBtn.innerHTML = '<i class="fas fa-trash-alt fa-spin" style="font-size:20px; color:black"></i>'
+    var delText = document.createTextNode("")
     delBtn.setAttribute("class", "btn")
     delBtn.setAttribute("onclick", "deleteItem(this)")
+    delBtn.setAttribute("class", "addButton")
     delBtn.appendChild(delText)
 
-    
+
     // create edit button
-    var editBtn = document.createElement("button")
-    var editText = document.createTextNode("Edit")
-    editBtn.appendChild(editText)    
+    var editBtn = document.createElement("button")  
+    editBtn.innerHTML = '<i class="fas fa-pencil-alt fa-spin" style="font-size:20px; color:black"></i>';
+    var editText = document.createTextNode('')
+    editBtn.setAttribute("class", "addButton")
+
+    editBtn.appendChild(editText)
 
     // delBtn.setAttribute("class", "btn")
-    
-    editBtn.setAttribute("onclick","editItem(this)")
-    
 
- 
+    editBtn.setAttribute("onclick", "editItem(this)")
+
+
+
     li.appendChild(delBtn)
     li.appendChild(editBtn)
 
-    
+
     list.appendChild(li)
 
     todo_item.value = ""
@@ -40,19 +48,19 @@ function addTodo(){
     console.log(li)
 }
 
-function deleteItem(e){
+function deleteItem(e) {
     e.parentNode.remove()
- }
+}
 
- function editItem(e) {
-     var val = e.parentNode.firstChild.nodeValue;
-     var editValue = prompt("Enter the edit value", val)
+function editItem(e) {
+    var val = e.parentNode.firstChild.nodeValue;
+    var editValue = prompt("Enter the edit value", val)
 
-     e.parentNode.firstChild.nodeValue = editValue
-     }
+    e.parentNode.firstChild.nodeValue = editValue
+}
 
 
 
- function deleteAll(){
+function deleteAll() {
     list.innerHTML = ""
- }
+}
